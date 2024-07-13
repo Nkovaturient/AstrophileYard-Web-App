c2619d1f6467178e68d4df9e86e3260f95d81b02
unknown <nk8981398@gmail.com>
2024-07-12 23:37:57 -0700

    cors options config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+        '/auth/google': {
+            target: 'https://astroyard-backend.onrender.com',
+            changeOrigin: true,
+            secure: false,
+        },
+    },
+},
 })
