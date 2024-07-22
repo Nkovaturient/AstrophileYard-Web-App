f1b8fe17bdf155fafd18aacd4815e22b7d0357cb
unknown <nk8981398@gmail.com>
2024-07-22 09:17:43 -0700

    css for gallery/musicaly

--- frontend/src/pages/Gallery/Gallery.jsx
+++ frontend/src/pages/Gallery/Gallery.jsx
@@ -6,6 +6,8 @@ import { toast } from 'react-toastify'
 import GallerySlider from '../../components/GallerySlider/GallerySlider.jsx'
 import Musicaly from '../Musicaly/Musicaly.jsx'
 import Spinner from '../../components/Spinner/Spinner.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faCameraAlt, faCompass, faSatellite, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
 
 const Gallery = () => {
     const{url, loading, image}=useContext(storeContext);
@@ -41,7 +43,7 @@ const Gallery = () => {
     <div className="misc-container">
     <div className='gallery'>
         <div className="gallery-header">
-            <h2>Cosmic Gallery: <span>Gallerxy</span></h2>
+            <h2><FontAwesomeIcon icon={faStarHalfStroke}/> Cosmic Gallery: &nbsp; <span>Gallerxy </span> <FontAwesomeIcon icon={faStarHalfStroke}/></h2>
             <p>Immerse yourself in this awe-inspiring photos from the observable corners of the galaxy, nebula, star clusters taken by astrophotographers around the world. </p>
             { loading ? <p>Loading..please wait</p> : ''}
         </div>
@@ -141,6 +143,42 @@ const Gallery = () => {
                 <p>Lorem ipsum dolor sit, amet consectetu Numquam dolores accusantium voluptas at.</p>
             </div>
         </div>
+        <div className="grid-cards" id='card15'>
+            <div className="content-overlay">
+                <h5>title</h5>
+                <p>Lorem ipsum dolor sit, amet consectetu Numquam dolores accusantium voluptas at.</p>
+            </div>
+        </div>
+        <div className="grid-cards" id='card16'>
+            <div className="content-overlay">
+                <h5>title</h5>
+                <p>Lorem ipsum dolor sit, amet consectetu Numquam dolores accusantium voluptas at.</p>
+            </div>
+        </div>
+        <div className="grid-cards" id='card17'>
+            <div className="content-overlay">
+                <h5>title</h5>
+                <p>Lorem ipsum dolor sit, amet consectetu Numquam dolores accusantium voluptas at.</p>
+            </div>
+        </div>
+        <div className="grid-cards" id='card18'>
+            <div className="content-overlay">
+                <h5>title</h5>
+                <p>Lorem ipsum dolor sit, amet consectetu Numquam dolores accusantium voluptas at.</p>
+            </div>
+        </div>
+        <div className="grid-cards" id='card19'>
+            <div className="content-overlay">
+                <h5>title</h5>
+                <p>Lorem ipsum dolor sit, amet consectetu Numquam dolores accusantium voluptas at.</p>
+            </div>
+        </div>
+        <div className="grid-cards" id='card20'>
+            <div className="content-overlay">
+                <h5>title</h5>
+                <p>Lorem ipsum dolor sit, amet consectetu Numquam dolores accusantium voluptas at.</p>
+            </div>
+        </div>
         </div>
         {/* <div className="pagination">
             <button className="prev">Previous</button>
@@ -156,6 +194,18 @@ const Gallery = () => {
             <button className="next">Next</button>
         </div> */}
         
+    </div>
+    <div className="info">
+        <div className="info-header">
+            <h5>Curious to know more? Check out this amazing webs for mindblowing info and daily updates.</h5>
+            <em>The Internet abounds with information. Its the curiousity and desire to learn thats scarce.</em>
+        </div>
+        <div className="info-links">
+            <a href="https://www.lpi.usra.edu/education/skytellers/constellations/ ">Constellations Cast <FontAwesomeIcon icon={faCompass}/></a>
+            <a href="https://www.amnh.org/exhibitions/journey-to-the-stars/educator-resources/stars/olbers-paradox#:~:text=In%201823%2C%20the%20German%20astronomer,beyond%20a%20sufficiently%20great%20distance.">Why is the sky dark at night? <FontAwesomeIcon icon={faSatellite}/></a>
+            <a href="https://welcome.astrobin.com">Astrophotographers Astrobin <FontAwesomeIcon icon={faCameraAlt}/></a>
+        </div>
+
     </div>
     </div>
     </>
--- frontend/src/pages/Musicaly/Musicaly.jsx
+++ frontend/src/pages/Musicaly/Musicaly.jsx
@@ -2,14 +2,14 @@ import React, { useContext, useEffect, useState } from 'react'
 import './Musicaly.css'
 // import './Musicaly.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeadphonesAlt, faMusic } from '@fortawesome/free-solid-svg-icons'
-import {storeContext} from '../../Context/storeContext.jsx'
+import { faArrowAltCircleDown, faArrowCircleDown, faArrowDown, faHeadphonesAlt, faMusic } from '@fortawesome/free-solid-svg-icons'
+import { storeContext } from '../../Context/storeContext.jsx'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 // import { Player } from 'react-spotify-web';
 
 const Musicaly = () => {
-  const{url, loading, setLoading}=useContext(storeContext);
+  const { url, loading, setLoading } = useContext(storeContext);
 
   const [playlistData, setPlaylistData] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -35,26 +35,26 @@ const Musicaly = () => {
 
   return (
     <>
-      
-    <div className="musical-container">
-      <h3>Tune in.. <FontAwesomeIcon icon={faHeadphonesAlt}/> <FontAwesomeIcon icon={faMusic}/></h3>
-      {playlistData ? (
-        <div>
-          <img src={playlistData.images[0].url} alt={playlistData.name} />
-          <h2>{playlistData.name}</h2>
-          <ul>
-            {playlistData.tracks.map((track) => (
-              <li key={track.uri}>
-                <span>{track.title}</span> - {track.artist}
-                <button onClick={() => playTrack(track.uri)}>Play</button>
-                {isPlaying && currentTrack === track.uri && (
-                  <button onClick={pauseTrack}>Pause</button>
-                )}
-              </li>
-            ))}
-          </ul>
-          {/* Optional Spotify playback */}
-          {/* {isPlaying && currentTrack && (
+
+      <div className="musical-container">
+        <h3>Tune in.. <FontAwesomeIcon icon={faHeadphonesAlt} /> <FontAwesomeIcon icon={faMusic} /></h3>
+        {playlistData ? (
+          <div>
+            <img src={playlistData.images[0].url} alt={playlistData.name} />
+            <h2>{playlistData.name}</h2>
+            <ul>
+              {playlistData.tracks.map((track) => (
+                <li key={track.uri}>
+                  <span>{track.title}</span> - {track.artist}
+                  <button onClick={() => playTrack(track.uri)}>Play</button>
+                  {isPlaying && currentTrack === track.uri && (
+                    <button onClick={pauseTrack}>Pause</button>
+                  )}
+                </li>
+              ))}
+            </ul>
+            {/* Optional Spotify playback */}
+            {/* {isPlaying && currentTrack && (
             <Player
               uri={currentTrack}
               width={300}
@@ -63,37 +63,47 @@ const Musicaly = () => {
               // Optional props like controls, onPlaybackChange, etc.
             />
           )} */}
-        </div>
-      ) : (
-        <>
-        <div className="load">
-        <p style={{color: '#fff'}}>Reload to view episodes!</p>
-        <iframe style={{"borderRadius":"12px", "height":"352" }}
-         src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7EF8wVxBVhG?utm_source=generator"
-         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-</div>
-         <div className="embed">
-         <div className="episodes">
-    <button className="episode" data-spotify-id="spotify:playlist:37i9dQZF1DXbIeCFU20wRm">
-      Atmospheric Sci-fi Soundtracks
-    </button>
-    <button className="episode" data-spotify-id="spotify:episode:6yiYRLDbBQhv3CXoQPfjMh">
-      The Smallest Ideas in the Universe
-    </button>
-    <button className="episode" data-spotify-id="spotify:playlist:37i9dQZF1DWXLeA8Omikj7">
-      Brain Food
-    </button>
-    <button className="episode" data-spotify-id="spotify:episode:2h70hGYz97ivJuTWR2Ax8M">
-     Travelling through Space and Time
-    </button>
-  </div>
+          </div>
+        ) : (
+          <>
+            <div className="load">
+              <div className="load-quote">
+                <em>"Music gives  Soul to the Universe, wings to the mind, flight to the imaginaion and life to Everything. "</em>
+              </div>
 
-  <div id="embed-iframe"></div>
-  </div>
-        </>
-      )}
-    </div>
-   
+              <iframe style={{ "borderRadius": "12px", "height": "352" }}
+                src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7EF8wVxBVhG?utm_source=generator"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <br />
+            <hr />
+
+            {/* <div className="load-quote">
+            <em>"Music gives a Soul to the Universe, wings to the mind, flight to the imaginaion and life to Everything. "</em>
+          </div> */}
+            <p style={{ color: '#fff' }}>Reload the webpage to view episodes! <FontAwesomeIcon icon={faArrowAltCircleDown}/></p>
+            <div className="embed">
+              <div className="episodes">
+                <button className="episode" data-spotify-id="spotify:playlist:37i9dQZF1DXbIeCFU20wRm">
+                  Atmospheric Sci-fi Soundtracks
+                </button>
+                <button className="episode" data-spotify-id="spotify:episode:6yiYRLDbBQhv3CXoQPfjMh">
+                  The Smallest Ideas in the Universe
+                </button>
+                <button className="episode" data-spotify-id="spotify:playlist:37i9dQZF1DWXLeA8Omikj7">
+                  Brain Food
+                </button>
+                <button className="episode" data-spotify-id="spotify:episode:2h70hGYz97ivJuTWR2Ax8M">
+                  Travelling through Space and Time
+                </button>
+              </div>
+
+              <div id="embed-iframe"></div>
+            </div>
+          </>
+        )}
+      </div>
+
     </>
   );
 };
